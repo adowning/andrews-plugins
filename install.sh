a295c42eadd0f0beca99af546b02e464ecd275b9
adowning <ashdowning@gmail.com>
2019-10-07 21:21:21 -0500

    a

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # init submodules to install defaults kuzzle plugins
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 
+git pull
+
+npm install
+
+pm2 restart API
+
+echo 'x' > shit.txt 
 # install dependencies for all enabled plugins
-for PLUGIN in ./plugins/enabled/*; do
-  if [ -d "${PLUGIN}" ]; then
-    ( cd "${PLUGIN}" && npm install )
-  fi
-done
+#for PLUGIN in ./plugins/enabled/*; do
+#  if [ -d "${PLUGIN}" ]; then
+#    ( cd "${PLUGIN}" && npm install )
+#  fi
+#done
 
